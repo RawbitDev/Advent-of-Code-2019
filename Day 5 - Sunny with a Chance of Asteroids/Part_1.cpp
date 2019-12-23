29fa9b041a370f53f328b27f30c7027260f6c184
Ramon <Ramon@CompTech-Walther.de>
2019-12-24 00:57:58 +0100

    Added final solution for day 5

---
+++ Day 5 - Sunny with a Chance of Asteroids/Part_1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+void execOpcode(vector <int>* intcode) {
+    unsigned int i=0;
+    while( i<intcode->size() ) {
+
+        int opcode = intcode->at(i)%100;
+        int* param1 = nullptr;
+        int* param2 = nullptr;
+        int* param3 = nullptr;
+
+        int paramMode1 = (intcode->at(i)/100)%10;
+        int paramMode2 = (intcode->at(i)/1000)%100;
+        int paramMode3 = (intcode->at(i)/10000)%1000;
+
+        if(paramMode1) { // Single parameter opcodes
+            param1 = new int(intcode->at(i+1)); // Immediate mode
+        } else {
+            param1 = &(intcode->at(intcode->at(i+1))); // Position mode
+        }
+
+        if(opcode <= 2) { // Two parameter opcodes
+            if(paramMode2) {
+                param2 = new int(intcode->at(i+2));
+            } else {
+                param2 = &(intcode->at(intcode->at(i+2)));
+            }
+        }
+
+        if(opcode <= 2) { // Three parameter opcodes
+            if(paramMode3) {
+                param3 = new int(intcode->at(i+3)); // Immediate mode
+            } else {
+                param3 = &(intcode->at(intcode->at(i+3))); // Position mode
+            }
+        }
+
+        switch (opcode)
+        {
+            case 1: { // Addition
+                *param3 = *param1 + *param2;
+                i+=4;
+                break;
+            }
+
+            case 2: { // Multiplication
+                *param3 = *param1 * (*param2);
+                i+=4;
+                break;
+            }
+
+            case 3: { // Input
+                int tmpInput = 0;
+                cout << "> ";
+                cin >> tmpInput;
+                *param1 = tmpInput;
+                i+=2;
+                break;
+            }
+
+            case 4: { // Output
+                cout << *param1 << endl;
+                i+=2;
+                break;
+            }
+
+            case 99: { // Halt
+                return;
+            }
+
+            default: {
+                throw runtime_error("ERROR: Encountered invalid opcode!" );
+            }
+        }
+    }
+    cerr << "ERROR: Opcode 99 to halt is missing!" << endl;
+}
+
+int main(int argc, char *argv[]) {
+    string file_path = "input.txt";
+    if(argc > 1) {
+        file_path = argv[1];
+    }
+    ifstream file(file_path);
+    if (file) {
+        stringstream linestream;
+        vector <int> intcode;
+        string line="", current="";
+        while(getline(file, current, ',')) {
+            intcode.push_back(stoi(current));
+        }
+        execOpcode(&intcode);
+    } else {
+        cerr << "Cannot open given file!" << endl;
+    }
+    return 0;
+}
+
